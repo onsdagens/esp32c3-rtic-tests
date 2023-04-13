af758f2e6ccb0d20115ecf58b2f3b5df90527080
onsdagens <pawdzi-7@student.ltu.se>
2023-04-13 15:46:32 +0200

    pulling in some dependency patches

--- examples/async-channel-no-sender.rs
+++ examples/async-channel-no-sender.rs
@@ -3,7 +3,7 @@
 #![no_main]
 #![no_std]
 //#![deny(warnings)]
-#![deny(unsafe_code)]
+//#![deny(unsafe_code)]
 #![deny(missing_docs)]
 #![feature(type_alias_impl_trait)]
 
@@ -13,6 +13,7 @@ use panic_rtt_target as _;
 mod app {
     use rtt_target::{rtt_init_print, rprintln};
     use rtic_sync::{channel::*, make_channel};
+    use esp32c3_hal::{riscv};
 
     #[shared]
     struct Shared {}
@@ -25,7 +26,19 @@ mod app {
     fn init(_: init::Context) -> (Shared, Local) {
         rtt_init_print!();
         let (_s, r) = make_channel!(u32, CAPACITY);
+       // unsafe{riscv::asm::nop()};
 
+        //the pointer is pc, so it's as if the instruction read is unaligned causing an exception, and when the pointer is dereferenced in exception handler 
+        //it panics
+
+        //but why is instruction read unaligned
+        //and this makes sense since 0x42000xxxx is program memory
+
+        //could it be that compressed instructions are causing this? eg. pc is pointing at compressed instruction, which is 2 bytes, causing the misaligned access.
+
+        //why is the nop needed?? must look into it
+        //exception is thrown otherwise with some misaligned access, so it doesn't even get to exception just panic
+        //this worked fine before...
         receiver::spawn(r).unwrap();
 
         (Shared {}, Local {})
